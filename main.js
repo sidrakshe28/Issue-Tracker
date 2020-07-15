7efd1cbbf5b7fb740e2cf157bd7098b9e0ee9675
Siddhika Rakshe <siddhikarakshe@gmail.com>
2020-07-15 14:38:10 +0530

    added code to close the issue

--- main.js
+++ main.js
@@ -29,6 +29,19 @@ function serveIssue(e) {
 
   e.preventDefault();
 }
+function setStatusClosed(id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+  
+    for (var i = 0; i < issues.length; i++) {
+      if (issues[i].id == id) {
+        issues[i].status = 'Closed';
+      }
+    }
+  
+    localStorage.setItem('issues', JSON.stringify(issues));
+  
+    fetchIssues();
+  }
  
  
  function fetchIssues() {
