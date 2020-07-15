834ae71877b51493cc8bc025a59fde3871e9c57c
Siddhika Rakshe <siddhikarakshe@gmail.com>
2020-07-15 14:39:27 +0530

    added code to delete the issue

--- main.js
+++ main.js
@@ -42,6 +42,21 @@ function setStatusClosed(id) {
   
     fetchIssues();
   }
+  function deleteIssue(id) {
+    var issues = JSON.parse(localStorage.getItem('issues'));
+  
+    for (var i = 0; i < issues.length; i++) {
+      if (issues[i].id == id) {
+        issues.splice(i, 1);
+      }
+    }
+  
+    localStorage.setItem('issues', JSON.stringify(issues));
+  
+    fetchIssues();
+  }
+
+
  
  
  function fetchIssues() {
